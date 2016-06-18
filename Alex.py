cc5522e82e170c9428de3971112205365c9115b5
LeMoNVeNoM <owenbrad@live.co.uk>
2016-06-18 08:37:54 +0000

    Add files via upload

--- Alex.py
+++ Alex.py
@@ -1,1 +1,14 @@
+# Importing Neccessary Packages
+import os, operator, turtle, tkinter
+from time import sleep
+import conversationPoss
+import saveInput
 
+# Alex
+print("Alex: Hello. My name is Alex.")
+while True:
+    userInput = input("You: ")
+    conversationPoss.execute(userInput)
+    saveInput.execute(userInput.lower())
+    print(reply)
+    
